666b08b9beaeb80b175e4842b00758e38164b015
DFStechBox <dfosterstewart@gmail.com>
2023-06-16 16:30:31 -0700

    first commit

---
+++ src/Content.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const Content = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One pound sugar",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "One dozen ggs",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "One loaf bread",
+    },
+    {
+      id: 4,
+      checked: false,
+      item: "One pound rice",
+    },
+  ]);
+
+  return (
+    <main>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input type="checkbox" checked={item.checked} />
+
+            <label>{item.item}</label>
+            <button>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default Content;
